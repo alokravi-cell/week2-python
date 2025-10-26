aaa26cf0e7c19fee1f71fd063941df55e16e94b3
alokravi-cell <alok.ravi@gmail.com>
2025-10-26 22:07:18 +0530

    Assignment of total bill per person

---
+++ Ass2.py
@@ -0,0 +1,4 @@
+tot_bill = int(input("Please enter total bill amount: "))
+tot_ppl = int(input("Please enter total number of people: "))
+bill_per = tot_bill/tot_ppl
+print("Hey!!! You need to pay Rs.", bill_per)
